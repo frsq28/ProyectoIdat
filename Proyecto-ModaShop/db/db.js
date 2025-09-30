bed610e9f0e157863d1ebb56b9cbbd7481736708
Franz <frsuarez1234@gmail.com>
2025-09-29 19:03:35 -0500

    ignorado

--- Proyecto-ModaShop/db/db.js
+++ Proyecto-ModaShop/db/db.js
@@ -1,14 +1,17 @@
-
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: "postgres",
+  host: "localhost",
+  database: "ModaShop",
+  password: "Franzsq123",   
+  port: 5432,
 });
 
 pool.connect()
   .then(() => console.log(" Conectado a PostgreSQL"))
-  .catch(err => console.error("Error al conectar a PostgreSQL:", err));
+  .catch(err => console.error(" Error al conectar a PostgreSQL:", err));
 
 module.exports = pool;
 
